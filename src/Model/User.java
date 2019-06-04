e48070e805d923e02770e0f104de41633e07b8be
hstdra <hstdra@gmail.com>
2019-06-04 09:32:41 +0700

    Initial commit

---
+++ src/Model/User.java
@@ -0,0 +1,12 @@
+package Model;
+
+import java.util.Date;
+
+public class User {
+    private String userID;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String avatar;
+    private Date dob;
+}
