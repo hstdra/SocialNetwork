abb5e315fb655cb3d842f2f82253f5f3046b4bbb
cityhunter132 <anhpbtde130050@fpt.edu.vn>
2019-06-15 22:49:18 +0700

    Merge branch 'master' of C:\Users\Pbtan\IdeaProjects\SocialNetwork with conflicts.

--- web/assets/js/chat_tab.js
+++ web/assets/js/chat_tab.js
@@ -1,5 +1,7 @@
-function changeColor(){
-    Array.from(document.querySelectorAll('button')).map(function(button) {
-        button.style.borderColor="green";
-    })
+function changeColor(_this) {
+    _this.style.boxShadow = "0px 7px red";
+}
+
+function oldColor(_this) {
+    _this.style.boxShadow = "0px 0px red";
 }
